46fa897f5aa7220a2a5d7134c92d60da1334591b
bsokat <barissokat@gmail.com>
2017-03-03 09:34:54 +0300

    Remove and Control Session

--- SessionManagement/Controllers/HomeController.cs
+++ SessionManagement/Controllers/HomeController.cs
@@ -18,14 +18,17 @@ namespace SessionManagement.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            Session.Remove("Owner");
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = Session["Owner"];
-
+            if (Session["Owner"] == null)
+                ViewBag.Message = "Session is null";
+            else
+                ViewBag.Message = "Session is " + Session["Owner"];
             return View();
         }
     }
