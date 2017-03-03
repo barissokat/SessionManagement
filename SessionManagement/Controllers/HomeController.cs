dccd6bec23917306a6b9f2faad55fe87525717ad
bsokat <barissokat@gmail.com>
2017-03-03 10:16:50 +0300

    Read, Remove and Control Application object

--- SessionManagement/Controllers/HomeController.cs
+++ SessionManagement/Controllers/HomeController.cs
@@ -11,8 +11,8 @@ namespace SessionManagement.Controllers
         public ActionResult Index()
         {
             //Sessions
-            Session.Add("Owner", "Baris Sokat");
-            Session["Owner2"] = "Baris Sokat";
+            //Session.Add("Owner", "Baris Sokat");
+            //Session["Owner2"] = "Baris Sokat";
 
             //Application Objects
             HttpContext.Application.Add("Owner", "Baris Sokat");
@@ -24,17 +24,29 @@ namespace SessionManagement.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-            Session.Remove("Owner");
+            //Sessions
+            //Session.Remove("Owner");
+
+            //Application Objects
+            HttpContext.Application.Remove("Owner");
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            if (Session["Owner"] == null)
-                ViewBag.Message = "Session is null";
+            //Sessions
+            //if (Session["Owner"] == null)
+            //    ViewBag.Message = "Session is null";
+            //else
+            //    ViewBag.Message = "Session is " + Session["Owner"];
+            //return View();
+
+            //Application Objects
+            if (HttpContext.Application["Owner"] == null)
+                ViewBag.Message = "Application is null";
             else
-                ViewBag.Message = "Session is " + Session["Owner"];
+                ViewBag.Message = "Application is " + HttpContext.Application["Owner"];
             return View();
         }
 
