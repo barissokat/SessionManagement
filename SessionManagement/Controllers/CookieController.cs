1d8ef197d526759fa99cfa4830c002942a6e20b5
bsokat <barissokat@gmail.com>
2017-03-04 14:04:48 +0300

    Create Cookie

---
+++ SessionManagement/Controllers/CookieController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionManagement.Controllers
+{
+    public class CookieController : Controller
+    {
+        public ActionResult Create()
+        {
+            HttpCookie cookieUser = new HttpCookie("user", "baris");
+            HttpContext.Response.Cookies.Add(cookieUser);
+            return View();
+        }
+    }
+}
